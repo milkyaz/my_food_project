1437a70619c93c00d566eade4335d08336577fa3
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-08-12 17:52:48 +0400

    fix basename

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
-import { Contact } from "./pages/Contact.jsx";
-import { About } from "./pages/About.jsx";
-import { Root } from "./pages/root.jsx";
-import { Category } from "./pages/Category.jsx";
-import { ErrorPage } from "./pages/error-page.jsx";
-import { Home } from "./pages/Home.jsx";
-import { Recipe } from "./pages/Recipe.jsx";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Category } from "./pages/Category";
+import { ErrorPage } from "./pages/error-page";
+import { Recipe } from "./pages/Recipe";
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route basename="/my_food_project">
-          <Route path="/" element={<Root />} errorElement={<ErrorPage />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/category/:name" element={<Category />} />
-          <Route path="/meal/:id" element={<Recipe />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </>
-    )
+  return (
+    <>
+      <Router basename="/my_food_project">
+        <Header />
+        <main className="container content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/category/:name" element={<Category />} />
+            <Route path="/meal/:id" element={<Recipe />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </>
   );
-
-  return <RouterProvider router={router} />;
 }
 
 export default App;
